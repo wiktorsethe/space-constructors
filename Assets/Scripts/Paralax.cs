14697a747ca743e03fa6f7958a52fe000ad34f01
wiktorsethe <115111728+wiktorsethe@users.noreply.github.com>
2023-09-01 19:00:14 +0200

    parallax

---
+++ Assets/Scripts/Paralax.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    private float lengthX;
+    private float lengthY;
+    private float startPosX;
+    private float startPosY;
+    private Camera mainCam;
+    [SerializeField] private float parallaxEffect;
+
+    private void Start()
+    {
+        startPosX = transform.position.x;
+        startPosY = transform.position.y;
+        lengthX = GetComponent<SpriteRenderer>().bounds.size.x;
+        lengthY = GetComponent<SpriteRenderer>().bounds.size.y;
+        mainCam = Camera.main;
+
+    }
+    private void Update()
+    {
+        float tempX = (mainCam.transform.position.x * (1 - parallaxEffect));
+        float distX = (mainCam.transform.position.x * parallaxEffect);
+
+        float tempY = (mainCam.transform.position.y * (1 - parallaxEffect));
+        float distY = (mainCam.transform.position.y * parallaxEffect);
+
+        transform.position = new Vector3(startPosX + distX, startPosY + distY, transform.position.z);
+
+        if (tempX > startPosX + lengthX) { startPosX += lengthX; }
+        else if (tempX < startPosX - lengthX) { startPosX -= lengthX; }
+
+        if (tempY > startPosY + lengthY) { startPosY += lengthY; }
+        else if (tempY < startPosY - lengthY) { startPosY -= lengthY; }
+    }
+}
